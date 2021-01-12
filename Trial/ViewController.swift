6850d02d00c66429a03a3a6c2da9d22f25816395
Cora-Jane Meade <corajanemeade@gmail.com>
2021-01-12 21:14:02 +0000

    initial commit

---
+++ Trial/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Trial
+//
+//  Created by My Apps on 12/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func working(){
+        print("working")
+    }
+}
+
